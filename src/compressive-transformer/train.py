b04584ea072cee08fe015556bd7f530891a30569
rll2396 <reid@cs.utexas.edu>
2021-10-02 18:08:15 -0500

    Add WikiText-2 and PennTreebank datasets

--- src/compressive-transformer/train.py
+++ src/compressive-transformer/train.py
@@ -3,12 +3,18 @@ from compressive_transformer_pytorch.autoregressive_wrapper import Autoregressiv
 
 import random
 import tqdm
-import gzip
+import zipfile
 import numpy as np
+import os
+import requests
 import torch
 import torch.optim as optim
+from torch import Tensor
 from torch.nn import functional as F
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader, Dataset, dataset
+from torchtext import datasets
+from torchtext.data.utils import get_tokenizer
+from torchtext.vocab import build_vocab_from_iterator
 
 # constants
 
@@ -38,29 +44,7 @@ def decode_token(token):
 def decode_tokens(tokens):
     return ''.join(list(map(decode_token, tokens)))
 
-# instantiate model
-
-model = CompressiveTransformer(
-    num_tokens = 256,
-    dim = 512,
-    depth = 8,
-    seq_len = SEQ_LEN,
-    mem_len = SEQ_LEN,
-    cmem_len = SEQ_LEN // 4,
-    heads = 8,
-    memory_layers = [6,7,8]
-)
-
-model = AutoregressiveWrapper(model)
-model.cuda()
-
-# prepare enwik8 data
-
-with gzip.open('./data/enwik8.gz') as file:
-    X = np.fromstring(file.read(int(95e6)), dtype=np.uint8)
-    trX, vaX = np.split(X, [int(90e6)])
-    data_train, data_val = torch.from_numpy(trX), torch.from_numpy(vaX)
-
+# prepare data
 class TextSamplerDataset(Dataset):
     def __init__(self, data, seq_len, segments):
         super().__init__()
@@ -77,10 +61,66 @@ class TextSamplerDataset(Dataset):
     def __len__(self):
         return self.data.size(0) // self.total_len
 
-train_dataset = TextSamplerDataset(data_train, SEQ_LEN, NUM_SEGMENTS)
-val_dataset   = TextSamplerDataset(data_val, SEQ_LEN, NUM_SEGMENTS)
+dataset_name = 'enwik8'
+
+if dataset_name == 'enwik8':
+    enwik8_path = "./data/enwik8.zip"
+    os.makedirs("data", exist_ok=True)
+    if not os.path.exists(enwik8_path):
+        response = requests.get("https://data.deepai.org/enwik8.zip")
+        with open(enwik8_path, 'wb') as f:
+            f.write(response.content)
+
+    with zipfile.ZipFile(enwik8_path, 'r') as archive:
+        X = np.fromstring(archive.read('enwik8')[:int(95e6)], dtype=np.uint8)
+        trX, vaX = np.split(X, [int(90e6)])
+        train_data, val_data = torch.from_numpy(trX), torch.from_numpy(vaX)
+    num_tokens = 256
+    lookup_fun = decode_tokens
+
+else:
+    if dataset_name == 'WikiText-2':
+        dataset_obj = datasets.WikiText2
+    else:
+        dataset_obj = datasets.PennTreebank
+    train_iter = dataset_obj(root='data', split='train')
+    tokenizer = get_tokenizer('basic_english')
+    vocab = build_vocab_from_iterator(map(tokenizer, train_iter), specials=['<unk>'])
+    vocab.set_default_index(vocab['<unk>'])
+
+    def data_process(raw_text_iter: dataset.IterableDataset) -> Tensor:
+        """Converts raw text into a flat Tensor."""
+        data = [torch.tensor(vocab(tokenizer(item)), dtype=torch.long) for item in raw_text_iter]
+        return torch.cat(tuple(filter(lambda t: t.numel() > 0, data)))
+
+    train_iter, val_iter, _ = dataset_obj(root='data')
+    train_data = data_process(train_iter)
+    val_data = data_process(val_iter)
+    num_tokens = len(vocab)
+    lookup_fun = vocab.lookup_tokens
+
+train_dataset = TextSamplerDataset(train_data, SEQ_LEN, NUM_SEGMENTS)
+val_dataset   = TextSamplerDataset(val_data, SEQ_LEN, NUM_SEGMENTS)
 train_loader  = cycle(DataLoader(train_dataset, batch_size = BATCH_SIZE))
 val_loader    = cycle(DataLoader(val_dataset, batch_size = BATCH_SIZE))
+
+# instantiate model
+
+model = CompressiveTransformer(
+    num_tokens = num_tokens,
+    dim = 512,
+    depth = 8,
+    seq_len = SEQ_LEN,
+    mem_len = SEQ_LEN,
+    cmem_len = SEQ_LEN // 4,
+    heads = 8,
+    gru_gated_residual = False,
+    one_kv_head = False,
+    memory_layers = None
+)
+
+model = AutoregressiveWrapper(model)
+model.cuda()
 
 # optimizer
 
@@ -104,19 +144,19 @@ for i in tqdm.tqdm(range(NUM_BATCHES), mininterval=10., desc='training'):
             optim.step()
             optim.zero_grad()
 
-    if i % VALIDATE_EVERY == 0:
+    if (i+1) % VALIDATE_EVERY == 0:
         model.eval()
         with torch.no_grad():
             for loss, aux_loss, _ in model(next(val_loader), return_loss = True):
                 print(f'validation loss: {loss.item():.4f}')
 
-    if i % GENERATE_EVERY == 0:
+    if (i+1) % GENERATE_EVERY == 0:
         model.eval()
         inp = random.choice(val_dataset)[:-1]
         inp = inp[:PRIME_LENGTH]
-        prime = decode_tokens(inp)
+        prime = lookup_fun(inp.cpu().numpy())
         print(f'%s \n\n %s', (prime, '*' * 100))
 
         sample = model.generate(inp, GENERATE_LENGTH)
-        output_str = decode_tokens(sample)
+        output_str = lookup_fun(sample.cpu().numpy())
         print(output_str)
