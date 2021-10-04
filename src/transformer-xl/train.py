1119d8605af99ddd521a998306b322ea88a79ac0
Matthew Kelleher <kelleher@cs.utexas.edu>
2021-10-03 20:48:12 -0500

    added figures

--- src/transformer-xl/train.py
+++ src/transformer-xl/train.py
@@ -17,12 +17,12 @@ from utils.exp_utils import create_exp_dir
 from utils.data_parallel import BalancedDataParallel
 
 parser = argparse.ArgumentParser(description='PyTorch Transformer Language Model')
-parser.add_argument('--data', type=str, default='../../data/enwik8/',
+parser.add_argument('--data', type=str, default='../../data/penn/',
                     help='location of the data corpus')
-parser.add_argument('--dataset', type=str, default='enwik8',
+parser.add_argument('--dataset', type=str, default='ptb',
                     choices=['wt103', 'lm1b', 'enwik8', 'text8', 'wt2', 'ptb'],
                     help='dataset name')
-parser.add_argument('--n_layer', type=int, default=8,
+parser.add_argument('--n_layer', type=int, default=16,
                     help='number of total layers')
 parser.add_argument('--n_head', type=int, default=8,
                     help='number of heads')
@@ -57,7 +57,7 @@ parser.add_argument('--lr', type=float, default=0.00025,
                     help='initial learning rate (0.00025|5 for adam|sgd)')
 parser.add_argument('--mom', type=float, default=0.0,
                     help='momentum for sgd')
-parser.add_argument('--scheduler', default='cosine', type=str,
+parser.add_argument('--scheduler', default='constant', type=str,
                     choices=['cosine', 'inv_sqrt', 'dev_perf', 'constant'],
                     help='lr scheduler to use.')
 parser.add_argument('--warmup_step', type=int, default=0,
@@ -72,7 +72,7 @@ parser.add_argument('--clip_nonemb', action='store_true',
                     help='only clip the gradient of non-embedding params')
 parser.add_argument('--max_step', type=int, default=2000,
                     help='upper epoch limit')
-parser.add_argument('--batch_size', type=int, default=32,
+parser.add_argument('--batch_size', type=int, default=16,
                     help='batch size')
 parser.add_argument('--batch_chunk', type=int, default=1,
                     help='split batch into chunks to save memory')
